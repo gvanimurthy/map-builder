7c56bb8efecc02634602be0f55aa72ecc17aad54
Vani Ganapa <gvanimurthy@gmail.com>
2017-04-11 13:11:54 +1000

    trying hot reload

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,31 @@
+var webpack = require('webpack');
 module.exports = {
-	module: {
-		loaders: [
+    entry: {
+        main: ['webpack-dev-server/client?http://localhost:8080','webpack/hot/dev-server','./app/main.jsx']
+    },
+    output: {
+        path: __dirname,
+        publicPath: 'http://localhost:8080/',
+        filename: 'dist/[name].js'
+    },
+    devtool: 'source-map',
+    
+    module: {
+        loaders: [
             {
-                test: /\.js$/,
-                loader: 'jsx',
-                exclude: /node_modules/
+                test: /\.jsx$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: 'babel-loader',
+                query: {
+                  cacheDirectory: true,
+                  presets: ['react', 'es2015']
+                }
+
             }
         ]
-	},
-	entry: {
-		main: ['webpack-dev-server/client?http://localhost:8080','./app/index.jsx']
-	},
-	output: {
-		filename: 'dist/[name].js'
-	}
-};
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin()
+    ],
+    devServer: { inline: true, hot: true, contentBase: './' }
+}
